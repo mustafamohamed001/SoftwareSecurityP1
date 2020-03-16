b464c131b68615f189226fbca1ad7346a5492389
MustafaMohamed001 <mustafa.mohamed@ufl.edu>
2020-03-16 03:18:37 -0400

    Removal of deprecated user model, fixed login backend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,70 +1,87 @@
-var mongoose = require('mongoose');
 var settings = require('../config/config');
 var express = require('express');
 var jwt = require('jsonwebtoken');
 var router = express.Router();
-var User = require('../models/Users');
 const sqlite3 = require('sqlite3').verbose();
 var bcrypt = require('bcrypt-nodejs');
 
 router.post('/register', function(req, res) {
-    if (!req.body.username || !req.body.password) {
-      res.json({success: false, msg: 'Please pass username and password.'});
-    } else {
-      var newUser = new User({
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email,
-        amount: 0
+    if (!req.body.username || !req.body.password || !req.body.email) {
+      res.status(400).json({success: false, msg: 'Please pass username, email, and password.'});
+    } 
+    else {
+      var username = req.body.username;
+      var password = req.body.password;
+      var email = req.body.email;
+      bcrypt.genSalt(10, function (err, salt) {
+        if (err) {
+          res.status(400).json({success: false, msg: 'Salt cannot generate'});
+        }
+        bcrypt.hash(password, salt, null, function (err, hash) {
+            if (err) {
+              res.status(400).json({success: false, msg: 'Hash unsuccessful'});
+            }
+            password = hash;
 
-      });
-      // save the user
-      newUser.save(function(err) {
-        
-        if (err != '0a0a') {
-          return res.status(400).json({success: false, msg: 'Username already exists.'});
-        }
-        else{
-          res.status(200).json({success: true, msg: 'Successful created new user.'});
-        }
+            //========= adding new user info to database ===================
+                const insertuser = 'INSERT INTO USERS (username, password, email, amount) VALUES (' + '\'' + username + '\'' + ', ' + '\'' + password + '\'' + ', ' + '\'' +  email + '\'' + ', ' + 0 + ')';
+                const sqllocation = __dirname.slice(0,__dirname.lastIndexOf('/')) + '/controllers/users.db'
+                const db = new sqlite3.Database(sqllocation);
+                db.all(insertuser, (err, rows) => {
+                    if (!err) {
+                        console.log('User has been inserted!');
+                        res.status(200).json({success: true, msg: 'Successful created new user.'});
+                    }
+                    else {
+                        console.log(err);
+                        res.status(400).json({success: false, msg: 'Username already exists.'});
+                    }
+                });
+            //======================= end ===========================
+        });
       });
     }
-  });
+});
 
 router.post('/login', function(req, res) {
-      var user = null;
-      var err = null;
-      const finduser = 'SELECT username FROM USERS WHERE username = \'' + req.body.username + '\'';
-      const sqllocation = __dirname.slice(0,__dirname.lastIndexOf('/')) + '/controllers/users.db'
-      const db = new sqlite3.Database(sqllocation);
-      db.all(finduser, (err, userrow) => {
-          if(!err){
-            if (userrow.length == 0) {
-              res.status(400).send({success: false, msg: 'Authentication failed. User not found.'});
-            } 
-            else {
-              user = userrow[0].username;
+  var user = null;
+  var err = null;
+  const finduser = 'SELECT username FROM USERS WHERE username = \'' + req.body.username + '\'';
+  const sqllocation = __dirname.slice(0,__dirname.lastIndexOf('/')) + '/controllers/users.db'
+  const db = new sqlite3.Database(sqllocation);
+  db.all(finduser, (err, userrow) => {
+      if(!err){
+        if (userrow.length == 0) {
+          res.status(400).send({success: false, msg: 'Authentication failed. User not found.'});
+        } 
+        else {
+          user = userrow[0].username;
 
-              // check if password matches
-              const findpass = 'SELECT password FROM USERS WHERE username = \'' + req.body.username + '\'';
-              db.all(findpass, (err, passrow) => {
-
-                if (passrow && !err) {
+          // check if password matches
+          const findpass = 'SELECT password FROM USERS WHERE username = \'' + req.body.username + '\'';
+          db.all(findpass, (err, passrow) => {
+            if(!err){
+              bcrypt.compare(req.body.password, passrow[0].password, function (err, isMatch) {
+                if (isMatch && !err) {
                   // if user is found and password is right create a token
                   var token = jwt.sign({username:user}, settings.secret, {expiresIn: '1h'});
                   // return the information including token as JSON
-                  res.json({success: true, token: 'JWT ' + token});
-                } 
-                else {
+                  res.status(200).json({success: true, token: 'JWT ' + token});
+                }
+                else{
+                  console.log(err);
                   res.status(400).send({success: false, msg: 'Authentication failed. Wrong password.'});
                 }
-              });
+              }); 
             }
-          }
-          else {
+            else{
               console.log(err);
-          }
-      });
+              res.status(400).send({success: false, msg: 'Authentication failed.'});
+            }
+          });
+        }
+      }
   });
+});
 
   module.exports = router;
