4986fe2ec5f3a0f3d3e31cc997abaacc1847566d
MustafaMohamed001 <mustafa.mohamed@ufl.edu>
2020-03-25 16:51:44 -0400

    site redirect

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -32,9 +32,10 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/dashboard">Dashboard</Nav.Link>
-                    <Nav.Link href="/signout">Sign Out</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+                        <Nav.Link href="https://minip1-vulnerable-272103.appspot.com/Home">Vulnerable Site</Nav.Link>
+                        <Nav.Link href="/signout">Sign Out</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
                 <Navbar.Collapse className="justify-content-end">
@@ -51,6 +52,9 @@ const Header = () => {
             <Navbar.Brand href="/">{websiteName}</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="https://minip1-vulnerable-272103.appspot.com/Home">Vulnerable Site</Nav.Link>
+                </Nav>
             </Navbar.Collapse>
             <Navbar.Collapse className="justify-content-end">
                 <Navbar.Text>
